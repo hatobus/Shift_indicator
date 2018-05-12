e63ce44a75396baaa9e67bf991a5b4603c3496d6
hatobus <s1240056@gmail.com>
2018-05-12 09:42:23 +0900

    [Update] add Shift register

---
+++ SRC/7segLED/src/7seg.ino
@@ -0,0 +1,39 @@
+#define SRCLK (5)
+#define RCLK (6)
+#define SER (7)
+
+
+void setup() {
+  pinMode(SRCLK, OUTPUT);
+  pinMode(RCLK,  OUTPUT);
+  pinMode(SER,   OUTPUT);
+}
+
+// 点灯パターン(1=点灯)
+const uint8_t PATTERNS[] = {
+  0b11111100, // 0
+  0b01100000, // 1
+  0b11011010, // 2
+  0b11110010, // 3
+  0b01100110, // 4
+  0b10110110, // 5
+  0b10111110, // 6
+  0b11100100, // 7
+  0b11111110, // 8
+  0b11110110, // 9
+};
+
+void loop() {
+  
+  int max_pattern = sizeof(PATTERNS)/sizeof(PATTERNS[0]);
+  for (uint8_t i=0; i<max_pattern; i++) {
+    // 8ビット分のデータをシフトレジスタへ送る
+    shiftOut(SER, SRCLK, LSBFIRST, PATTERNS[i]); 
+
+    // シフトレジスタの状態をストアレジスタへ反映させる
+    digitalWrite(RCLK,  LOW);
+    digitalWrite(RCLK,  HIGH);
+
+    delay(200);
+  }
+}
